3a9ee40a3132b5e6afa0bf22769bc4aec1574d18
urxt <80124020+urxt@users.noreply.github.com>
2022-04-29 19:34:28 -0400

    cleaning tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_01(self):
+        # your site here
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
+
