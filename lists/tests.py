8e1701195981cbd8e5d1f52bd120d24b9f68649f
urxt <80124020+urxt@users.noreply.github.com>
2022-04-30 21:54:36 -0400

    shorter POST test and fixed blank items

--- lists/tests.py
+++ lists/tests.py
@@ -33,22 +33,20 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
-    
-    """
-    def test_root_url_resolves_to_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html')
-    """
