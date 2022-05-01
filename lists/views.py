27547adfef376f1616e680fea104aa73ef35d104
urxt <80124020+urxt@users.noreply.github.com>
2022-05-01 14:33:16 -0400

    redirect after POST

--- lists/views.py
+++ lists/views.py
@@ -9,5 +9,6 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
 
-    return render(request, 'home.html') 
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items}) 
 
